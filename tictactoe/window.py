5d7db87a07e2f45fbc36854fc6c54d1759668d3a
Abid Chowdhury <77800165+abid12508@users.noreply.github.com>
2024-10-26 19:55:46 -0400

    Update window.py

--- tictactoe/window.py
+++ tictactoe/window.py
@@ -31,41 +31,7 @@ class window:
             button.grid(row=i//3, column=i%3)
             self.buttonlist.append(button)
             
-        """button0 = tk.Button(button_Frame, text=" ", borderwidth=1, font=self.font, 
-                            width=3, command=lambda: self.game.mark(button0, 0))
-        button1 = tk.Button(button_Frame, text=" ", borderwidth=1, font=self.font, 
-                            width=3, command=lambda: self.game.mark(button1, 1))
-        button2 = tk.Button(button_Frame, text=" ", borderwidth=1, font=self.font, 
-                            width=3, command=lambda: self.game.mark(button2, 2))
-
-        button3 = tk.Button(button_Frame, text=" ", borderwidth=1, font=self.font, 
-                            width=3, command=lambda: self.game.mark(button3, 3))
-        button4 = tk.Button(button_Frame, text=" ", borderwidth=1, font=self.font, 
-                            width=3, command=lambda: self.game.mark(button4, 4))
-        button5 = tk.Button(button_Frame, text=" ", borderwidth=1, font=self.font, 
-                            width=3, command=lambda: self.game.mark(button5, 5))
-
-        button6 = tk.Button(button_Frame, text=" ", borderwidth=1, font=self.font, 
-                            width=3, command=lambda: self.game.mark(button6, 6))
-        button7 = tk.Button(button_Frame, text=" ", borderwidth=1, font=self.font, 
-                            width=3, command=lambda: self.game.mark(button7, 7))
-        button8 = tk.Button(button_Frame, text=" ", borderwidth=1, font=self.font, 
-                            width=3, command=lambda: self.game.mark(button8, 8))
-
-        
-
-        button0.grid(row=0, column=0)
-        button1.grid(row=0, column=1)
-        button2.grid(row=0, column=2)
-
-        button3.grid(row=1, column=0)
-        button4.grid(row=1, column=1)
-        button5.grid(row=1, column=2)
-
-        button6.grid(row=2, column=0)
-        button7.grid(row=2, column=1)
-        button8.grid(row=2, column=2)"""
-
+      
         button_Frame.place(x=3)
 
 
